9703ee0798ebb65c0a70b43dd16df6b42ac07b3d
vtphan <vphan@memphis.edu>
2014-03-29 23:47:02 -0500

    improved way to read tab-separated file

---
+++ matreader.py
@@ -0,0 +1,78 @@
+'''
+Author: Vinhthuy Phan, 2014
+'''
+
+#-----------------------------------------------------------------------------
+class Row:
+   def __init__(self, keys, values, sep):
+      ks = [ i.strip() for i in keys.split(sep) ]
+      vs = [ i.strip() for i in values.split(sep) ]
+      self.data = {}
+      for i, k in enumerate(ks):
+         try:
+            self.data[k] = float(vs[i])
+         except:
+            self.data[k] = vs[i]
+
+   def __getitem__(self, key):
+      if key in self.data:
+         return self.data[key]
+      return None
+
+
+#-----------------------------------------------------------------------------
+class Rows:
+   ''' Default setting assumes the file is tab-delimited '''
+   def __init__(self, header, lines, sep):
+      self.rows = [ Row(header, line, sep) for line in lines ]
+      self.keys = [ line.split(sep)[0].strip() for line in lines ]
+      self.sep = sep
+      self.header = header
+
+   def __len__(self):
+      return len(self.rows)
+
+   def __iter__(self):
+      self.i = -1
+      return self
+
+   def next(self):
+      if self.i < len(self.rows)-1:
+         self.i += 1
+         return self.rows[self.i]
+      else:
+         raise StopIteration
+
+   def ignore(self, *val):
+      try:
+         idx = set([ self.keys.index(v) for v in val ])
+         n = Rows(self.header, [], self.sep)
+         n.rows = [ self.rows[i] for i in range(len(self.rows)) if i not in idx ]
+         n.keys = [ self.keys[i] for i in range(len(self.keys)) if i not in idx ]
+         return n
+      except:
+         return self
+
+   def __getitem__(self, key):
+      return [r[key] for r in self]
+
+
+#-----------------------------------------------------------------------------
+
+def read(filename, sep='\t'):
+   with open(filename, 'rU') as f:
+      # remove all empty and comments (starting with #) in data
+      lines = [ line.strip() for line in f.readlines() if line.strip() and (line.strip()[0] != '#')]
+
+   header = lines.pop(0).strip()
+   return Rows(header, lines, sep)
+
+#-----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+   data = read("complexity.txt")
+   # print [r['D12'] for r in data.ignore('NT_167185.1.fasta', 'NT_167196.1.fasta', 'NT_077528.2.fasta')]
+   # print [r['ID'] for r in data]
+   print data['D12']
+   print data.ignore('NT_167185.1.fasta','NT_167196.1.fasta')['D12']
+
