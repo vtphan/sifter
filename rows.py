ded3978fb0f4fcd12b1e3558255677aa70226b21
vtphan <vphan@memphis.edu>
2014-09-19 22:06:08 -0500

    cluster data by a column

--- rows.py
+++ rows.py
@@ -32,6 +32,11 @@ class Rows(object):
    def __len__(self):
       return len(self.rows)
 
+   ''' self[key] returns column "key" '''
+   def __getitem__(self, key):
+      return [r[key] for r in self]
+
+   ''' enumerate through self.rows '''
    def __iter__(self):
       self.i = -1
       return self
@@ -43,18 +48,30 @@ class Rows(object):
       else:
          raise StopIteration
 
+   ''' return a dict keyed by values of "col" '''
+   def cluster(self, col):
+      if col not in self.header:
+         return None
+      c = {}
+      for r in self:
+         key = r[col]
+         if key not in c:
+            c[key] = Rows(self.header, [])
+            c[key].keys = self.keys
+         c[key].rows.append(r)
+      return c
+
    def ignore(self, *val):
       try:
          idx = set([ self.keys.index(v) for v in val ])
          n = Rows(self.header, [])
          n.rows = [ self.rows[i] for i in range(len(self.rows)) if i not in idx ]
          n.keys = [ self.keys[i] for i in range(len(self.keys)) if i not in idx ]
+         n.header = self.header
          return n
       except:
          return self
 
-   def __getitem__(self, key):
-      return [r[key] for r in self]
 
 
 #-----------------------------------------------------------------------------
